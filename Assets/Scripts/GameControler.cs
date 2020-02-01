7a018845e312df35ecb82bc31bae7d0b5dc5687e
Aluno <gabrieldias2032@gmail.com>
2020-02-01 11:41:24 -0300

    vs 0.1

---
+++ Assets/Scripts/GameControler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControler : MonoBehaviour
+{
+    [SerializeField] GameObject virus, canvas;
+    float cd;
+    bool Spawn;
+
+    void Start()
+    {
+        Spawn = true;
+        cd = 2f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        GerarVirus();
+    }
+    private void GerarVirus()
+    {
+        if (Spawn)
+        {
+            Instantiate(virus, GerarPosicao(), Quaternion.identity, canvas.transform);
+            Spawn = !Spawn;
+            StartCoroutine(Cooldown(cd));
+        }
+
+    }
+    private Vector3 GerarPosicao()
+    {
+        int typePosition = 0;
+        switch (typePosition)
+        {
+            case 0:
+                return new Vector3(-70, Random.Range(450, -110));
+                break;
+        }
+        return new Vector3(0,0,0);
+    }
+    IEnumerator Cooldown(float cd) 
+    {
+        yield return new WaitForSeconds(cd);
+        Spawn = true;
+    }
+    
+}
---
+++ Assets/Scripts/Virus1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus1 : MonoBehaviour
+{
+    [SerializeField] private Vector2 coracaoposition;
+    [SerializeField] private float speed, pontos, multiplicadorTamanho;
+
+    void Start()
+    {
+        coracaoposition = transform.parent.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedReal = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, coracaoposition, speedReal);
+        transform.localScale = new Vector3(pontos * multiplicadorTamanho, pontos * multiplicadorTamanho);
+    }
+}
