45b935a8c322deef3ac8604828cf61520cb67752
Aluno <gabrielaragaofds@gmail.com>
2020-02-01 17:32:33 -0300

    Menu

---
+++ Assets/MenuBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    UI UI;
+    int coolDown = 2;
+    bool ativado = true;
+    string[] cenas = new string[] { "SampleScene", "Creditos" };
+
+    private void Fade()
+    {
+        if (ativado)
+        {
+            ativado = !ativado;
+            UI = GetComponent<UI>();
+            UI.Fades(false, coolDown, Random.Range(0, 2));
+        }
+    }
+    public void BotaoInciar() 
+    {
+        Fade();
+        StartCoroutine(CoolDown(coolDown,cenas[0]));        
+    }
+    public void BotaoDiverso()
+    {
+        Fade();
+        StartCoroutine(CoolDown(coolDown,cenas[1]));
+    }
+
+    
+    IEnumerator CoolDown(int cd,string cena) 
+    {
+        yield return new WaitForSeconds(cd);
+        SceneManager.LoadScene(cena);
+
+    }
+}
