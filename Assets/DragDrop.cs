1bda2b4fe493fa83dfeb046dede9325154b498a6
Aluno <aluno@naverecife.local>
2020-02-01 11:57:49 -0300

    vs 0.2

---
+++ Assets/DragDrop.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler {
+
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake() {
+
+        canvas = GameObject.FindGameObjectWithTag("canva").GetComponent<Canvas>();
+
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+    }
+    Collider2D[] objetos;
+    private void Update()
+    {
+        objetos = Physics2D.OverlapCircleAll(transform.position, 1);
+        if (objetos.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
