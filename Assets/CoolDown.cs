517d8f64ca8ba62e120627cad40802181fbda79f
Aluno <gabrielaragaofds@gmail.com>
2020-02-02 10:45:21 -0300

    Mercado :)

---
+++ Assets/CoolDown.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoolDown : MonoBehaviour
+{
+    private float tempoCd, cd;
+    public bool podeClicar;
+
+    void Start()
+    {
+       podeClicar = true;
+       transform.GetChild(0).GetComponent<Image>().fillAmount = 0;
+        switch (transform.tag)
+        {
+            case "globuloBranco":
+                tempoCd = 5;
+                break;
+            case "tempoLoja":
+                tempoCd = 10;
+                break;
+            case "freezeLoja":
+                tempoCd = 20;
+                break;
+
+        }
+    }
+    void Update() 
+    {
+        if (cd >= 0)
+        {
+            cd -= Time.deltaTime;
+            transform.GetChild(0).GetComponent<Image>().fillAmount = cd / tempoCd;
+        }
+        else 
+        {
+            podeClicar = true;
+        }
+
+        
+    }
+    public void Clique() 
+    {
+        if(podeClicar)
+        {
+            podeClicar = !podeClicar;
+            cd = tempoCd;    
+        }
+    }
+}
